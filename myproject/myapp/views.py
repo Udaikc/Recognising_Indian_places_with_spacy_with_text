9ef4d51e01a8b9df966185bbb904ede1acfd0477
udaikc <udaikcsps@gmail.com>
2024-07-29 22:08:51 +0530

    Created a django app that reads and displays present in the form

---
+++ myproject/myapp/views.py
@@ -0,0 +1,19 @@
+import re
+import spacy
+from spacy.tokens import Span
+from spacy.util import filter_spans
+from spacy import displacy
+# Create your views here.
+from django.shortcuts import render
+from .forms import SentenceForm
+
+def input_form(request):
+    if request.method == 'POST':
+        form = SentenceForm(request.POST)
+        if form.is_valid():
+            sentence = form.cleaned_data['sentence']
+            return render(request, 'myapp/display.html', {'sentence': sentence})
+    else:
+        form = SentenceForm()
+    return render(request, 'myapp/input.html', {'form': form})
+
